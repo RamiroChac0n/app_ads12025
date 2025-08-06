d2cf12b5a2993d4c92bdbeef96e77912556f29d1
Ramiro Chacón <ramiro20010202@gmail.com>
2025-08-06 10:53:23 -0600

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { title } from 'node:process';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{    
+    path:'home',
+    title:'App | Home',
+    loadComponent: () => import('./home/pages/home/home.component'),
+}
+];
